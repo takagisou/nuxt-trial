8a0165ea650542eecbb4df0e346872022e6d7a79
sana <takagisou@outlook.com>
2019-05-06 05:25:05 +0900

    Fix Mxied Content

--- pages/color.vue
+++ pages/color.vue
@@ -15,7 +15,12 @@ import axios from 'axios'
 export default {
     async asyncData(){
         const hex = Math.floor(Math.random() * 16777215).toString(16)
-        const { data } = await axios.get(`http://www.thecolorapi.com/id?hex=${hex}`)
+        const { data } = await axios.get(`http://www.thecolorapi.com/id`,{
+            params: {
+                // ここにクエリパラメータを指定する
+                hex: hex // このようにパラメータを付けるとhttpsになる
+            }
+        })
         return { data }
     }    
 }
--- pages/scheme.vue
+++ pages/scheme.vue
@@ -15,7 +15,11 @@ import axios from 'axios'
 export default {
     async asyncData() {
         const hex = Math.floor(Math.random() * 16777215).toString(16)
-        const { data } = await axios.get(`http://www.thecolorapi.com/scheme?hex=${hex}`)
+        const { data } = await axios.get(`http://www.thecolorapi.com/scheme`, {
+            params: {
+                hex: hex
+            }
+        })
         return { data }
     }
 }

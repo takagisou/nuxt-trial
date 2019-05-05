ba7e9275729906f4816bd57c2ac05c78e000b17b
sana <takagisou@outlook.com>
2019-05-06 05:18:08 +0900

    SSR

---
+++ pages/color.vue
@@ -0,0 +1,23 @@
+<template>
+<section>
+    <div>
+        <span>color</span>
+        <span>|</span>
+        <span><nuxt-link to="/scheme">scheme</nuxt-link></span>
+    </div>
+
+    <div><img :src="data.image.named" /></div>
+</section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    async asyncData(){
+        const hex = Math.floor(Math.random() * 16777215).toString(16)
+        const { data } = await axios.get(`http://www.thecolorapi.com/id?hex=${hex}`)
+        return { data }
+    }    
+}
+</script>
+
